231e8ce13f3f5621f32d25a70168e44b3d8167ea
Alex McLeod <alexmcleod@stripe.com>
2020-04-04 20:32:24 -0700

    bonus additions and bug fixes

--- script.js
+++ script.js
@@ -5,45 +5,64 @@ window.onload = async function(){
       addForm.setAttribute("type","text");
       addForm.setAttribute("id","inputForm")
       addForm.setAttribute("placeholder","Sub reddit name here");
+      addForm.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+          document.getElementById("BUTTONID").click();
+        }
+      });
       document.body.appendChild (addForm);
   
       // Add a submit button
       var addButton = document.createElement('BUTTON');
+      addButton.setAttribute("id","BUTTONID");
       addButton.innerHTML = "Submit";
       addButton.addEventListener('click', async function(){
+        // Delete all existing posts
+        let arePosts = document.getElementById("allThePosts");
+        if (arePosts != null){
+          arePosts.remove();
+        };
+
+        // Get form input
         let formInput = document.getElementById("inputForm").value;
+
+        // Scrape Reddit
         let r = await fetch('https://www.reddit.com/r/'+ formInput +'/.json');
         let responseJSON = await r.json();
         
-          for(post of responseJSON.data.children) {
-            let header = document.createElement('h3');
-            let anchor = document.createElement('a');
-            let details = document.createElement('p');
-            let pic = document.createElement('img');
+        // Display all posts
+        let postHolder = document.createElement('div');
+        postHolder.setAttribute("id","allThePosts");
+        for(post of responseJSON.data.children) {
+          let header = document.createElement('h3');
+          let anchor = document.createElement('a');
+          let details = document.createElement('p');
+          let pic = document.createElement('img');
 
-            header.style.border = "thin solid gray";
-            header.style.backgroundColor = "lightgray";
-            header.style.padding = "5px";
+          header.style.border = "thin solid gray";
+          header.style.backgroundColor = "lightgray";
+          header.style.padding = "5px";
 
-            anchor.style.color = "black";
-            anchor.style.fontFamily = "arial,sans-serif";
-            anchor.style.textDecoration = "none";
-            details.style.color = "gray";
-            details.style.fontFamily = "arial,sans-serif";
-            details.style.fontSize = "x-small";
+          anchor.style.color = "black";
+          anchor.style.fontFamily = "arial,sans-serif";
+          anchor.style.textDecoration = "none";
+          details.style.color = "gray";
+          details.style.fontFamily = "arial,sans-serif";
+          details.style.fontSize = "x-small";
 
-            anchor.href = post.data.url;
-            anchor.innerText = post.data.title;
-            details.innerText = post.data.author_fullname;
-            pic.src = post.data.thumbnail;
+          anchor.href = post.data.url;
+          anchor.innerText = post.data.title;
+          details.innerText = post.data.author_fullname;
+          pic.src = post.data.thumbnail;
 
-            header.appendChild(anchor);
-            header.appendChild(details);
-            header.appendChild(pic);
-    
-            document.body.appendChild(header);
-          }
-          
+          header.appendChild(anchor);
+          header.appendChild(details);
+          header.appendChild(pic);
+  
+          postHolder.appendChild(header);
+          document.body.appendChild(postHolder);        
+        }
+  
       });
       document.body.appendChild(addButton);
 
